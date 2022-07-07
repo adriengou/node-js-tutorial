748c0998617785dba35204ba95d616b6029eec84
adrien gouacide <adrien.gouacide@gmail.com>
2022-07-07 16:57:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+const { debug } = require("console");
 const http = require("http");
+const fs = require("fs");
 
 const port = process.env.PORT || 3000;
 
+//Functions
+
+//Server
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello, World!</h1>");
+  let path = "./src/";
+
+  switch (req.url) {
+    case "/":
+      path = path + "index.html";
+      break;
+
+    case "/about":
+      path = path + "about.html";
+      break;
+
+    case "/contact":
+      path = path + "contact-me.html";
+      break;
+
+    default:
+      path = path + "404.html";
+  }
+
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.setHeader("Content-Type", "text/html");
+    res.end(data);
+  });
 });
 
 server.listen(port, () => {
